86603bb7dea730c9fe65f57cd31e3ed5d09452af
ncorv <33473556+ncorv@users.noreply.github.com>
2018-10-17 19:08:46 -0400

    Update Main.java

--- src/com/election/ridings/Main.java
+++ src/com/election/ridings/Main.java
@@ -3,7 +3,7 @@ package com.election.ridings;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    // write your code here
         while(true)System.out.println("hi world");
     }
 }
